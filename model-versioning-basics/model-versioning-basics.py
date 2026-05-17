45239cf4f4c233ad8bcd2304276bd4975c58502b
Ath-07 <athshetwe@gmail.com>
2026-05-17 20:29:37 +0530

    Add solution for model-versioning-basics

---
+++ model-versioning-basics/model-versioning-basics.py
@@ -0,0 +1,12 @@
+def promote_model(models):
+    """
+    Decide which model version to promote to production.
+    """
+    # Write code here
+    best_model = max(
+        models,
+        key=lambda m: (m["accuracy"], -m["latency"], m["timestamp"])
+    )
+    
+    return best_model["name"]
+    pass
